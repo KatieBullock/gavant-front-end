ae421938f97da19771357b8922152ca61be3c010
KatieBullock <kathrynleahbullock@gmail.com>
2021-02-21 14:31:24 -0500

    added ability to search by full name and project

--- app/components/customers/filter.js
+++ app/components/customers/filter.js
@@ -1,12 +1,17 @@
 import Component from "@glimmer/component";
 
 export default class CustomersFilterComponent extends Component {
-  //To find results based on customers component model and if it includes the company value of the user searched query
+  //To find results based on customers component model and if it includes the company, full name, and project value of the user searched query
   get results() {
     let { model, query } = this.args;
 
     if (query) {
-      model = model.filter((customer) => customer.company.includes(query));
+      model = model.filter(
+        (customer) =>
+          customer.company.includes(query) ||
+          customer.fullName.includes(query) ||
+          customer.project.includes(query)
+      );
     }
 
     return model;
